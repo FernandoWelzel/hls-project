2935ce97c34a7c27f85ba5b9d15fececdece86dc
Fernando Welzel <fernando.welzel.br@gmail.com>
2023-11-15 10:51:14 +0100

    Fixed convolution for higher input channels

--- src/python/Conv2D.py
+++ src/python/Conv2D.py
@@ -24,7 +24,7 @@ class Conv2D:
         self.input_shape = input_shape
 
         # Internal matrices
-        self.weights = np.ones([kernel_size[0], kernel_size[1], input_shape[2], output_shape[2]], dtype=float)
+        self.weights = np.ones((kernel_size[0], kernel_size[1], input_shape[2], output_shape[2]), dtype=float)
         self.bias = np.ones(output_shape[2], dtype=float)
 
     # Calculates the convolutional output
@@ -33,43 +33,51 @@ class Conv2D:
         if(feature_map.ndim == 2):
             feature_map = feature_map.reshape((1, feature_map.shape[0], feature_map.shape[1]))
 
+        # Resizing weights matrix if only one channel
+        if(self.weights.ndim < 4):
+            self.weights = self.weights.reshape((self.kernel_size[0], self.kernel_size[1], self.input_shape[2], self.output_shape[2]))
+
         output = np.zeros(self.output_shape, dtype=float)
 
         # Output shapes
         rows = self.output_shape[0]
         columns = self.output_shape[1]
-        channels = self.output_shape[2]
+        channels_out = self.output_shape[2]
+
+        channels_in = self.input_shape[2]
 
         kernelX = self.kernel_size[0]
         kernelY = self.kernel_size[1]
 
         # Compute each channel
-        for c in range(channels):
+        for c_out in range(channels_out):
             # Compute each row
             for x in range(rows):
                 # Compute each column
                 for y in range(columns):
-                    sum = self.bias[c]
-                    out_of_bounds = False
+                    # Initializing sum
+                    sum = self.bias[c_out]
+                    
+                    # Iterating though input layers of the kernel
+                    for c_in in range(channels_in):
+                        # Iterate though X of kernel
+                        for m in range(kernelX):
+                            x_index = x+m-1
+                            x_out_of_bound = (x_index < 0) or (x_index >= self.input_shape[0])
 
-                    # Iterate though X of kernel
-                    for m in range(kernelX):
-                        x_index = x+m-1
-                        x_out_of_bound = (x_index < 0) or (x_index >= self.input_shape[0])
+                            # Iterate though Y of kernel
+                            for n in range(kernelY):
+                                y_index = y+n-1
+                                y_out_of_bound = (y_index < 0) or (y_index >= self.input_shape[1])
 
-                        # Iterate though Y of kernel
-                        for n in range(kernelY):
-                            y_index = y+n-1
-                            y_out_of_bound = (y_index < 0) or (y_index >= self.input_shape[1])
-
-                            out_of_bounds = x_out_of_bound or y_out_of_bound
-                            
-                            if(not out_of_bounds):
-                                sum += feature_map[c, x_index, y_index]
+                                out_of_bounds = x_out_of_bound or y_out_of_bound
+                                
+                                if(not out_of_bounds):
+                                    sum += self.weights[m, n, c_in, c_out]*feature_map[c_in, x_index, y_index]
                     
                     # Applies ReLU
                     if(sum < 0): sum = 0
             
-                    output[x, y, c] = sum
+                    output[x, y, c_out] = sum
 
         return output
--- src/python/model.py
+++ src/python/model.py
@@ -26,7 +26,44 @@ test_matrix_dense = np.array((
 # MaxPooling layer
 pool_model = MaxPooling2D(input_shape=(5, 5, 1), strides=(1, 1), pool_size=(3, 3))
 
-# Tests
+# Tests 1
 print(conv_model.forward(test_matrix_conv))
 print(dense_model.forward(test_matrix_dense))
 print(pool_model.forward(test_matrix_conv))
+
+# Changing weights
+conv_model.weights = np.array((
+    [0, 0, 0],
+    [0, 1, 0],
+    [0, 0, 0]
+))
+
+conv_model.bias = np.array((
+    [0, 0, 0, 0, 0]
+))
+
+# Tests 2
+print(conv_model.forward(test_matrix_conv))
+
+# 3 channel convolutional model
+conv_3C_model = Conv2D(kernel_size=(3, 3), output_shape=(5, 5, 2), input_shape=(5, 5, 3))
+
+test_matrix_conv_3C = np.array((
+   [[1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5]],
+   [[1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5]],
+   [[1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5]]
+))
+
+print(conv_3C_model.forward(test_matrix_conv_3C))
