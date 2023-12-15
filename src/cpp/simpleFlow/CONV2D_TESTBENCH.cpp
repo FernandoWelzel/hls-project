848891119b3196b54873b9c14b577d151b1922a4
Fernando Welzel <fernando.welzel.br@gmail.com>
2023-12-15 08:48:38 +0100

    Added synthesizable testbench

---
+++ src/cpp/simpleFlow/CONV2D_TESTBENCH.cpp
@@ -0,0 +1,49 @@
+// Specific definitions for this test
+#define ARCHITECTURE 1
+#define C_IN 1
+#define ROWS 3
+#define COLUMNS 3
+#define C_OUT 1
+#define K_X 3
+#define K_Y 3
+
+#include "types.hpp"
+#include "conv2d.hpp"
+
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+
+CCS_MAIN(int argc, char **argv) {
+    // Test variables
+    d_type data_in[C_IN*ROWS*COLUMNS];
+    d_type data_out[C_OUT*ROWS*COLUMNS];
+    c_type coeffs_in[C_OUT*C_IN*ROWS*COLUMNS];
+    c_type bias_in[C_OUT];
+
+    // Fill with arbitrary data
+    for(int i = 0; i < C_IN*ROWS*COLUMNS; i++) {
+        data_in[i] = 1;
+    }
+    
+    for(int j = 0; j < C_OUT*C_IN*ROWS*COLUMNS; j++) {
+        coeffs_in[j] = 5e-1;
+    }
+    
+    for(int k = 0; k < C_OUT; k++) {
+        bias_in[k] = 1;
+    }
+
+    // Test main loop
+	CCS_DESIGN(CONV_HARDWARE)(data_in, coeffs_in, bias_in, data_out);
+
+    // Printing ordered result
+    for(int i = 0; i < ROWS; i++) {
+        for(int j = 0; j < COLUMNS; j++) {
+            std::cout << data_out[i*ROWS+j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+	CCS_RETURN(0);
+}
